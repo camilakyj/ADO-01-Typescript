bc092c55c98332941fd72f071d243eabe9f78560
Camila <yagimaj@gmail.com>
2026-08-29 10:44:47 -0300

    Add files via upload

--- ex1.ts
+++ ex1.ts
@@ -1,16 +1,22 @@
 import promptSync from "prompt-sync";
 
-const prompt = promptSync(); 
+const prompt = promptSync();
 
-let tipo = prompt("Digite o tipo do ingresso (Meia ou Inteira): ");
-let quantidade = Number(prompt("Digite a quantidade de ingressos: "));
+function calcularTotal(tipo: string, quantidade: number): number {
+    let total: number;
 
-let total: number;
+    if (tipo == "Meia") {
+        total = 14.25 * quantidade;
+    } else {
+        total = 28.50 * quantidade;
+    }
 
-if (tipo == "Meia") {
-    total = 14.25 * quantidade;
-} else {
-    total = 28.50 * quantidade;
+    return total;
 }
 
-console.log("O total a pagar é R$: " + total);
+let tipo = prompt("Digite o tipo de ingresso (Meia ou Inteira): ");
+let quantidade = Number(prompt("Digite a quantidade de ingressos: "));
+
+let total = calcularTotal(tipo, quantidade);
+
+console.log("O total a pagar é: R$ "+ total);
